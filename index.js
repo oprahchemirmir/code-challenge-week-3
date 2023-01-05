adc00b6f9bd2eb79da37f63f6f787a1f4c9e0ab1
oprah <oprah.chemirmir@student.moringaschool.com>
2023-01-05 13:33:39 +0300

    done with challenge

--- index.js
+++ index.js
@@ -1,89 +1,76 @@
-let URL = 'http://localhost:3000/films'
-
-
-const listHolder = document.getElementById('films')
-document.addEventListener('DOMContentLoaded', ()=>{
-    document.getElementsByClassName('film item')[0].remove()
-    fetchMovies(URL)
-})
-
-//Create fetch function to get the data from the db.json
-function fetchMovies(URL){
-    fetch("https://oprahchemirmir.github.io/code-challenge-week-3/db.json")
-    .then(resp => resp.json())
-    .then(movies => {
-        movies.forEach(movie => {
-            displayMovie(movie)
+document.addEventListener("DOMContentLoaded", () => {
+    // getting elements from the DOM
+    const moviesListContainer = document.querySelector("#moviesList");
+    const movieDetailsContainer = document.querySelector("#movieDetails");
+  
+    // getting films data
+    fetch(URL)
+      .then((res) => res.json())
+      .then((data) => {
+        displayMoviesList(data.films);
+      });
+  
+    //   displaying movies titles
+    function displayMoviesList(data) {
+      data.map((movie) => {
+        const markUp = `<li class="list-group-item" id="movieList">${movie.title}</li>`;
+  
+        // append movie titles
+        moviesListContainer.insertAdjacentHTML("afterbegin", markUp);
+  
+        // show the first movie details on the list on load of the page
+        displayMovieDetails(movie);
+  
+        // show movie details on click of the movie titles
+        const movieList = document.querySelector("#movieList");
+        movieList.addEventListener("click", () => {
+          displayMovieDetails(movie);
         });
-    })
-}
-//function to display the titles of the movies as a list
-function displayMovie(movie){
-   
-    const list = document.createElement('li')
-    list.style.cursor="cell"
-    list.textContent= (movie.title)
-    listHolder.appendChild(list)
-    addClickEvent()
-}
-
-//Adding the click event listener
-function addClickEvent(){
-    let children=listHolder.children
-    
-
-    for(let i=0; i<children.length; i++){
-        let child=children[i]
-        // console.log(child) <= to check if have the right child
-
-        child.addEventListener('click',() => {
-            fetch(`${URL}/${i+1}`)
-
-            .then(res => res.json())
-            .then(movie => {
-                document.getElementById('buy-ticket').textContent = 'Buy Ticket'
-                setUpMovieDetails(movie)
-            })
-
-        })
+      });
     }
-}
-
-//Posting the movie details
-// the poster to be dispalyed on the div with poster id
-function setUpMovieDetails(funMovie){
+  
+    // movie details
+    function displayMovieDetails(movie) {
+      const markUp = `<img src="${movie.poster}" alt="">
+    <div class="hello">
+    <h2 id="movieTitle" class= "customtitles">${movie.title}</h2>
+    <p id="description">${movie.description}</p>
+    <p id="runtime">Length: <span>${movie.runtime} Minutes</span></p>
+    <p id="showtime">Show Time: <span>${movie.showtime}</span></p>
+    <p id="capacity">Theater capacity: <span>${movie.capacity}</span></p>
+    <p id="ticketssold">Tickets sold: <span>${movie.tickets_sold}</span></p>
+    <p id="availableTickets">Available tickets: <span>${
+      movie.capacity - movie.tickets_sold
+    }</span></p>
+    <button class="custombtn" id="buyTicket">buy ticket </button>
+   </div>`;
+  
+      movieDetailsContainer.innerHTML = "";
+      movieDetailsContainer.insertAdjacentHTML("afterbegin", markUp);
+  
+      const btn = movieDetailsContainer.querySelector("button");
+      btn.addEventListener("click", () => {
+        buyTicket(movie);
+      });
+    }
+  
+    // ticket buying functionality
     
-    const preview = document.getElementById('poster')
-    preview.src = funMovie.poster;
-//title
-    const movieTitle = document.querySelector('#title');
-    movieTitle.textContent = funMovie.title;
-    //runtime
-    const movieTime = document.querySelector('#runtime');
-    movieTime.textContent = `${funMovie.runtime} minutes`;
-
-    //description
-    const movieDescription = document.querySelector('#film-info');
-    movieDescription.textContent = funMovie.description;
-    //Showtime
-    const showTime = document.querySelector('#showtime')
-    showTime.textContent = funMovie.showtime;
-    // available tickets =capacity - tickets sold
-    const tickets  = document.querySelector('#ticket-number')
-    tickets.textContent = funMovie.capacity -funMovie.tickets_sold;
-}
-
-// //Sold out
-const btn = document.getElementById('buy-ticket')
-
-        btn.addEventListener('click', function(event){
-            let remainingTickets = document.querySelector('#ticket-number').textContent
-            event.preventDefault()
-            if(remainingTickets > 0){
-                document.querySelector('#ticket-number').textContent  = remainingTickets-1
-                
-            }
-            else if(parseInt(remTickets, 10)===0){
-                btn.textContent = 'Sold Out'
-            }
-    })
+    function buyTicket(movie) {
+      const ticketsSold = document.querySelector("#ticketssold");
+      let remainingTickets = movie.capacity - movie.tickets_sold;
+      const btn = movieDetailsContainer.querySelector("button");
+      const availableTickets = document.querySelector("#availableTickets");
+  
+      if (remainingTickets > 0) {
+        movie.tickets_sold++;
+        remainingTickets--;
+        btn.innerHTML = "buy ticket";
+      } else {
+        btn.innerHTML = "sold out";
+        btn.classList.add("soldOut");
+      }
+      ticketsSold.innerHTML = `Tickets sold: <span>${movie.tickets_sold}</span>`;
+      availableTickets.innerHTML = `Available tickets: <span>${remainingTickets}</span>`;
+    }
+  });
